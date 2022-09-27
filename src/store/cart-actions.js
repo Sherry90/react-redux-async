cf37d3b11e5828d19e10744f6a3dddabcff076ca
Sherry <hhlsdkm@gmail.com>
2022-09-27 12:28:36 +0900

    cart.changed

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -20,7 +20,12 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -46,7 +51,10 @@ export const sendCartData = (cart) => {
     const sendRequest = async () => {
       const response = await fetch(API_URL, {
         method: "PUT",
-        body: JSON.stringify(cart),
+        body: JSON.stringify({
+          items: cart.items,
+          totalQuantity: cart.totalQuantity,
+        }),
       });
 
       if (!response.ok) {
